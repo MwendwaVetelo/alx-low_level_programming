d840c7ea59f7302e7701b87da82778aac82e007f
MwendwaVetelo <p.t.mwendwa@gmail.com>
2023-02-17 15:26:03 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: is 0
+ */
+int main(void)
+{
+	char v;
+
+	int w;
+
+	v = 'a';
+	w = 0;
+	while
+		(w < 10)
+		{
+			putchar(w + '0');
+			w++;
+		}
+	while
+		(v <= 'f')
+		{
+			putchar(v);
+			v++;
+		}
+	putchar('\n');
+	return (0);
+
+}
