24513d32490a71ec041fc9dfb50aac7ac8132bf9
MwendwaVetelo <p.t.mwendwa@gmail.com>
2023-02-17 12:56:49 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: is 0
+ */
+int main(void)
+{
+	char v;
+
+	for (v = 'a' ; v <= 'z' ; v++)
+		putchar(v);
+	putchar('\n');
+	return (0);
+}
