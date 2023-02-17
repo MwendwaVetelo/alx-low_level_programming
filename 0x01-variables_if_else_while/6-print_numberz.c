95e7457416503bc6445225f22bb8f160644b6a32
MwendwaVetelo <p.t.mwendwa@gmail.com>
2023-02-17 14:58:08 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: is 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = '0' ; v <= 9 ; v++)
+	{
+		putchar(v + '0');
+	}
+	putchar('\n');
+	return (0);
+}
