5fc9b7c5668d642c9abd997412dff5a358a877c5
MwendwaVetelo <p.t.mwendwa@gmail.com>
2023-02-17 14:25:41 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: is 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
